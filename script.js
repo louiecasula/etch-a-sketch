8413f19755fc23a0c5fc5485e6fa0317b54cc7dc
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-06 11:58:55 -0500

    Working on 16x16 grid, needs work.

---
+++ script.js
@@ -0,0 +1,13 @@
+const board = document.querySelector('.board');
+
+let length = 16;
+
+for (let i = 0; i < length; i ++) {
+    let row = document.createElement('row');
+    for (let j = 0; j < length; j++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+    board.appendChild(row);
+}
