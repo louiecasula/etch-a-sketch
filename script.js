7c54be5b98ebab858aea695302040666af34e244
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-07 17:44:13 -0500

    Squares fit to container!

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const board = document.querySelector('.board');
 let length = 16;
 
 for (let i = 0; i < length; i ++) {
-    let row = document.createElement('row');
+    let row = document.createElement('div');
+    row.classList.add('row');
     for (let j = 0; j < length; j++) {
         let square = document.createElement('div');
         square.classList.add('square');
