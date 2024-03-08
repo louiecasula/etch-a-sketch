b010985d45a5dddbedce5d99601ef68c857b7f80
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-08 10:55:31 -0500

    Added slider bar for adjusting board dimensions.

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const board = document.querySelector('.board');
 
-let length = 16;
+const slider = document.getElementById("myRange");
 
-for (let i = 0; i < length; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < length; j++) {
-        let square = document.createElement('div');
-        square.addEventListener('click', function(){
-            square.style.cssText = 'background-color: black;';
-        })
-        square.classList.add('square');
-        row.appendChild(square);
-    }
-    board.appendChild(row);
-};
+const boardSize = document.querySelector('.boardSize');
+
+let createBoard = (length) => {
+    board.innerHTML = ``;
+    for (let i = 0; i < length; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < length; j++) {
+            let square = document.createElement('div');
+            square.addEventListener('click', function(){
+                square.style.cssText = 'background-color: black;';
+            })
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+        board.appendChild(row);
+    };
+}
+
+let updateBoardSize = (length) => {
+    boardSize.innerHTML = `<h3>${length} x ${length}</h3>`;
+}
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', function(){
@@ -23,3 +32,14 @@ clear.addEventListener('click', function(){
         squares[i].style.cssText = 'background-color: white';
     };
 });
+
+// How can we make this not repeat?
+let length = slider.value;
+createBoard(length);
+updateBoardSize(length);
+
+slider.oninput = function() {
+    length = this.value;
+    createBoard(length);
+    updateBoardSize(length);
+};
