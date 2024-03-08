0758c0543180b746b9fb74e1652c3c47b456bfe5
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-08 08:31:37 -0500

    Squares turn black on click. Board shadow darkened.

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const board = document.querySelector('.board');
 
 let length = 16;
 
-for (let i = 0; i < length; i ++) {
+for (let i = 0; i < length; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
     for (let j = 0; j < length; j++) {
         let square = document.createElement('div');
+        square.addEventListener('click', function(){
+            square.style.cssText = 'background-color: black;'
+        })
         square.classList.add('square');
         row.appendChild(square);
     }
